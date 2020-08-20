4f12d6ad7c5e94207649f2f4796bdb1e8ef574f8
Joseph Estabrook <JEstabrook@users.noreply.github.com>
2020-08-20 15:49:48 -0700

    adding util data

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='regulon-enrichment',
-      version='0.3.9a0',
+      version='0.3.10a0',
       description="""This package leverages pathway information and gene expression data to produce
         regulon-based protein activity scores""",
       long_description = """regulon-enrichment leverages pathway information and gene expression data to produce
@@ -11,8 +11,9 @@ setuptools.setup(name='regulon-enrichment',
       entry_points={'console_scripts':['enrich=enricher.enrich:main']},
       packages=setuptools.find_packages(
           exclude=["enricher.tests.*", "enricher.tests", "enricher.plot"]),
-      data_files=[('data',['data/PathwayCommons9.All.hgnc.sif.gz',
-                           'data/secondary_intx_regulon.pkl'])],
+      data_files=[('enricher/data',
+                   ['data/PathwayCommons9.All.hgnc.sif.gz', 'data/secondary_intx_regulon.pkl']
+                   )],
       url = 'https://github.com/JEstabrook/regulon-enrichment',
       classifiers = [
          "Programming Language :: Python :: 3",
