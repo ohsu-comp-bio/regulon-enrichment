1591c72321bc52fd6c25531dbc8a4b5724787dbb
Joseph Estabrook <JEstabrook@users.noreply.github.com>
2020-08-20 16:00:13 -0700

    adding data files attribute

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='regulon-enrichment',
-      version='0.3.10a0',
+      version='0.3.11a0',
       description="""This package leverages pathway information and gene expression data to produce
         regulon-based protein activity scores""",
       long_description = """regulon-enrichment leverages pathway information and gene expression data to produce
@@ -10,10 +10,11 @@ setuptools.setup(name='regulon-enrichment',
       author_email='estabroj@ohsu.edu',
       entry_points={'console_scripts':['enrich=enricher.enrich:main']},
       packages=setuptools.find_packages(
-          exclude=["enricher.tests.*", "enricher.tests", "enricher.plot"]),
-      data_files=[('enricher/data',
+          exclude=["enricher.tests.*", "enricher.tests", "enricher.plot.*","enricher.plot"]),
+      data_files=[('enricher',
                    ['data/PathwayCommons9.All.hgnc.sif.gz', 'data/secondary_intx_regulon.pkl']
                    )],
+      package_data={'enricher':['data/PathwayCommons9.All.hgnc.sif.gz', 'data/secondary_intx_regulon.pkl']},
       url = 'https://github.com/JEstabrook/regulon-enrichment',
       classifiers = [
          "Programming Language :: Python :: 3",
