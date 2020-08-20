3ff181a51dfd5db526cf47ed69defaf398b1a9ff
Joseph Estabrook <JEstabrook@users.noreply.github.com>
2020-08-20 14:52:28 -0700

    adding data to distribution

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='regulon-enrichment',
-      version='0.3.8a0',
+      version='0.3.9a0',
       description="""This package leverages pathway information and gene expression data to produce
         regulon-based protein activity scores""",
       long_description = """regulon-enrichment leverages pathway information and gene expression data to produce
@@ -11,11 +11,8 @@ setuptools.setup(name='regulon-enrichment',
       entry_points={'console_scripts':['enrich=enricher.enrich:main']},
       packages=setuptools.find_packages(
           exclude=["enricher.tests.*", "enricher.tests", "enricher.plot"]),
-      data_files=[('data',['data/Homo_sapiens.GRCh38.95.gtf.gz',
-                           'data/Mus_musculus.GRCm38.95.gtf.gz',
-                           'data/PathwayCommons9.All.hgnc.sif.gz',
-                           'data/primary_intx_regulon.pkl',
-                           'secondary_intx_regulon.pkl'])],
+      data_files=[('data',['data/PathwayCommons9.All.hgnc.sif.gz',
+                           'data/secondary_intx_regulon.pkl'])],
       url = 'https://github.com/JEstabrook/regulon-enrichment',
       classifiers = [
          "Programming Language :: Python :: 3",
