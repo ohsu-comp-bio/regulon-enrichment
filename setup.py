1150effbd670b82c899111d8d10bdcd63eda7a7b
Will Yashar <wmyashar@gmail.com>
2023-09-29 18:26:17 -0700

    removed entry points from the setup.py, added the __main__.py file and remove the main call from priori.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 import setuptools
 
 setuptools.setup(name='priori',
-      version='1.0.4',
+      version='1.0.6',
       description="""Priori predicts transcription factor activity from RNA sequencing data using prior, literature-supported regulatory relationship information.""",
       author='Joseph Estabrook, William Yashar, & Emek Demir',
       author_email='yashar@ohsu.edu',
-      entry_points={'console_scripts':['priori = priori.priori:main']},
       packages=setuptools.find_packages(
         where=".",
         include=['priori*'],
